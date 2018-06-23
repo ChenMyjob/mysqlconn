ddaa66529182072af2552b2da471f5755fcfefa7
cmy <cmy_job@163.com>
2018-06-24 00:57:34 +0800

    can connect mysql server

---
+++ mysqlconn/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace mysqlconn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_conn = new System.Windows.Forms.Button();
+            this.btn_seach = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_conn
+            // 
+            this.btn_conn.Location = new System.Drawing.Point(145, 226);
+            this.btn_conn.Name = "btn_conn";
+            this.btn_conn.Size = new System.Drawing.Size(75, 23);
+            this.btn_conn.TabIndex = 0;
+            this.btn_conn.Text = "连接";
+            this.btn_conn.UseVisualStyleBackColor = true;
+            this.btn_conn.Click += new System.EventHandler(this.btn_conn_Click);
+            // 
+            // btn_seach
+            // 
+            this.btn_seach.Location = new System.Drawing.Point(345, 225);
+            this.btn_seach.Name = "btn_seach";
+            this.btn_seach.Size = new System.Drawing.Size(75, 23);
+            this.btn_seach.TabIndex = 1;
+            this.btn_seach.Text = "查询";
+            this.btn_seach.UseVisualStyleBackColor = true;
+            this.btn_seach.Click += new System.EventHandler(this.btn_seach_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 459);
+            this.Controls.Add(this.btn_seach);
+            this.Controls.Add(this.btn_conn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_conn;
+        private System.Windows.Forms.Button btn_seach;
+    }
+}
+
---
+++ mysqlconn/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace mysqlconn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region  建立MySql数据库连接
+        /// <summary>
+        /// 建立数据库连接.
+        /// </summary>
+        /// <returns>返回MySqlConnection对象</returns>
+        public MySqlConnection getmysqlcon()
+        {
+            string M_str_sqlcon = "server=85.10.205.173:3306;user id=mengydz;password=Yangyes211;database=mengydz"; //根据自己的设置
+            MySqlConnection myCon = new MySqlConnection(M_str_sqlcon);
+            return myCon;
+        }
+        #endregion
+
+        #region  执行MySqlCommand命令
+        /// <summary>
+        /// 执行MySqlCommand
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        public void getmysqlcom(string M_str_sqlstr)
+        {
+            MySqlConnection mysqlcon = this.getmysqlcon();
+            mysqlcon.Open();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+            mysqlcom.ExecuteNonQuery();
+            mysqlcom.Dispose();
+            mysqlcon.Close();
+            mysqlcon.Dispose();
+        }
+        #endregion
+
+        #region  创建MySqlDataReader对象
+        /// <summary>
+        /// 创建一个MySqlDataReader对象
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        /// <returns>返回MySqlDataReader对象</returns>
+        public MySqlDataReader getmysqlread(string M_str_sqlstr)
+        {
+            MySqlConnection mysqlcon = this.getmysqlcon();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+            mysqlcon.Open();
+            MySqlDataReader mysqlread = mysqlcom.ExecuteReader(CommandBehavior.CloseConnection);
+            return mysqlread;
+        }
+        #endregion
+
+        private void btn_conn_Click(object sender, EventArgs e)
+        {
+            string constructorString = "server=db4free.net;User Id=mengydz;password=Yangyes211;Database=mengydz";
+            MySqlConnection myConnnect = new MySqlConnection(constructorString);
+            myConnnect.Open();
+            MySqlCommand myCmd = new MySqlCommand("insert into mupdate(value) values('优化了启动响应，增加了拍照，解决了一些已知问题')", myConnnect);
+            if (myCmd.ExecuteNonQuery() > 0)
+            {
+                //MessageBox.Show("sucess");
+            }
+            myCmd.Dispose();
+            myConnnect.Close();
+        }
+
+        private void btn_seach_Click(object sender, EventArgs e)
+        {
+            string constructorString = "server=db4free.net;User Id=mengydz;password=Yangyes211;Database=mengydz";
+            MySqlConnection myConnnect = new MySqlConnection(constructorString);
+            myConnnect.Open();
+            MySqlCommand myCmd = new MySqlCommand("SELECT value FROM `mupdate`", myConnnect);
+            MySqlDataReader reader = null;
+            reader = myCmd.ExecuteReader();
+            while (reader.Read())
+            {
+                MessageBox.Show(reader[0].ToString());
+            }
+
+            myCmd.Dispose();
+            myConnnect.Close();
+        }
+
+    }
+}
